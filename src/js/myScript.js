282fd36227152fa46b38a64267137a4b10d15641
yaroslav <yarius1979@gmail.com>
2016-01-26 15:07:00 +0200

    add myProject

---
+++ src/js/myScript.js
@@ -0,0 +1,12 @@
+﻿window.onload = function () {
+
+    var scrollUp = document.getElementById('button_toUp');
+
+    window.onscroll = function () {
+        if (window.pageYOffset > 0) {
+            scrollUp.style.display = 'block';
+        } else {
+            scrollUp.style.display = 'none';
+        }
+    };
+};
